c102e8adc9468e6e65accaf72afa3541a64654f2
teirzar <nik.ul@icloud.com>
2023-09-19 02:54:45 +0300

    изменено: config.py, other_functions.py
    config.py - удалена информация для старта бота, перенесена в other_functions.py/main
    
    other_functions.py - add_log - исправлена кодировка, start_stop - добавлена функция для воспроизведения во время старта/остановки бота, main - функция для старта бота, заполнены импорты

--- config.py
+++ config.py
@@ -1,15 +1,4 @@
-from aiogram import Bot, Dispatcher
 from db import DBconnect
-from aiogram.fsm.storage.memory import MemoryStorage
-from functions import get_token
-
-
-# переменные для запуска бота
-TOKEN = get_token()
-storage = MemoryStorage()
-bot = Bot(TOKEN)
-dp = Dispatcher(bot, storage=storage)
-
 
 # расположение файла с базой
 src = "db/db_bot.db"
--- functions/other_functions.py
+++ functions/other_functions.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 
 
 def get_token() -> str:
@@ -17,6 +19,31 @@ async def get_time() -> str:
 async def add_log(string) -> None:
     """Функция логирования.
     Вносит в txt файл данные в формате 'Время: событие' """
-    with open('logs/log.txt', 'a') as log:
+    with open('logs/log.txt', 'a', encoding='UTF-8') as log:
         text = f'{await get_time()}: {string}\n'
         log.write(text)
+
+
+def start_stop(is_stop=False):
+    text_message = f"Бот {'остановлен' if is_stop else 'запущен'}"
+
+    async def func(bot: Bot):
+        await add_log(f"!! {text_message} !!")
+        print(text_message)
+        await bot.send_message(210189427, text_message)
+
+    return func
+
+
+async def main() -> None:
+    """Старт бота"""
+    bot = Bot(token=get_token())
+    dp = Dispatcher(storage=MemoryStorage())
+
+    dp.startup.register(start_stop())
+    dp.shutdown.register(start_stop(is_stop=True))
+
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
