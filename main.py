7d61f89045be6267bc4eb27cb6ee316671fa13b1
teirzar <nik.ul@icloud.com>
2023-09-19 22:26:50 +0300

    исправлено: main.py
    main.py - мелкие правки для удобства разработки

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ if __name__ == "__main__":
     from functions import main
 
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Завершено по желанию пользователя")
+    except:
+        print("Завершено из-за ошибки")
