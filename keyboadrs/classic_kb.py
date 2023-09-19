c392ada71d958fb9d399634948c69e6ae0e4ce1b
teirzar <nik.ul@icloud.com>
2023-09-19 22:15:29 +0300

    добавлено: keyboards/classic_kb.py
    classic_kb.py - заполнен импорт, добавлена клавиатура kb_main_menu

---
+++ keyboadrs/classic_kb.py
@@ -0,0 +1,16 @@
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+
+def kb_main_menu():
+    """Основная клавиатура главного меню"""
+    kb = ReplyKeyboardBuilder()
+
+    kb.button(text="Мой профиль")
+    kb.button(text="Режим изучения")
+    kb.button(text="Обычный режим")
+    kb.button(text="Режим экзамена")
+    kb.button(text="Режим марафона")
+    kb.button(text="Случайный режим ")
+    kb.button(text="Работа над ошибками")
+    kb.adjust(1, 2, 2, 2)
+    return kb.as_markup(resize_keyboard=True, input_field_placeholder="Выберите режим")
