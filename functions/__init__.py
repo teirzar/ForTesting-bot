11b820b95d4f58c686adf58b8f646ace54c334f0
teirzar <nik.ul@icloud.com>
2023-09-21 00:26:56 +0300

    изменено: client_functions.py, functions/__init__.py
    client_functions.py - заполенен импорт, добавлено 4 новых функции:
    1) is_new_session - проверяет есть ли у пользователя активные сессии по данному выбранному режиму
    2) questions_generate - функция для генерации порядка вопросов, использует библиотеку random
    3) create_new_session - для создания новой сессии и загрузки созданного порядка вопросов в базу данных
    4) get_question - функция для работы с вопросами и ответами, генерирует текст вопроса и варианты ответов, также информацию о количестве ответов и о правильном ответе
    
    __init__.py - внесение новых функций

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,2 +1,2 @@
 from functions.other_functions import get_token, add_log, main
-from functions.client_functions import register, get_users
+from functions.client_functions import register, get_users, is_new_session, create_new_session, get_question
--- functions/client_functions.py
+++ functions/client_functions.py
@@ -1,4 +1,5 @@
-from config import users
+from config import users, sessions, questions_all, full_base
+from random import randint
 
 
 async def get_users() -> list:
@@ -12,3 +13,75 @@ async def get_users() -> list:
 async def register(user_id, name, personnel_number) -> None:
     """Функция для регистрации пользователя в базе данных"""
     users.write('id', 'name', 'personnel_number', values=f'{user_id}, "{name}", {personnel_number}')
+
+
+async def is_new_session(mode, user_id) -> bool:
+    """Проверяет, есть ли у пользователя запущенные тесты"""
+    return not bool(sessions.print_table('questions', where=f'mode = "{mode}" and user_id = {user_id} and status = 0'))
+
+
+def questions_generate(length, is_random=True) -> list:
+    """Функция генерации порядка вопросов"""
+    if not is_random:
+        return [str(i) for i in range(length)]
+
+    lst = []
+    while len(lst) != length:
+        num = randint(0, 204)
+        if str(num) in lst:
+            continue
+        lst.append(str(num))
+    return lst
+
+
+async def create_new_session(mode, user_id) -> None:
+    """Создает новую сессию для пользователя, загружает номера вопросов в базу данных"""
+    if not await is_new_session(mode, user_id):
+        sessions.update('status = 1', where=f'user_id = {user_id} and mode = "{mode}"')
+
+    is_random = True
+    match mode:
+
+        case "Режим изучения" | "Режим марафона":
+            is_random, length = False, 204
+
+        case "Обычный режим":
+            length = 20
+
+        case "Режим экзамена":
+            length = 10
+
+        case "Случайный режим":
+            length = 204
+
+        case "Работа над ошибками":
+            length = 1
+
+    questions = " ".join(questions_generate(length, is_random=is_random)).strip()
+
+    sessions.write('user_id', 'mode', 'questions', 'amount', 'status',
+                   values=f'{user_id}, "{mode}", "{questions}", {length}, 0')
+
+
+async def get_question(mode, user_id) -> tuple | str:
+    """Генерирует текст вопроса и количество ответов, а также номер верного ответа"""
+    res = sessions.print_table('questions', 'amount', where=f'user_id = {user_id} and mode = "{mode}" and status = 0')
+
+    if not res:
+        return "Ошибка, сессия не найдена, начните новую сессию!"
+
+    questions, amount = res[0]
+    number_current_question = amount - len(questions.split())
+    current_question = int(questions.split()[0])
+    len_answers = len(full_base[questions_all[current_question]])
+    text_msg = f"#{current_question}\nВопрос №{number_current_question}\n" \
+               f"(Осталось вопросов: {len(questions.split())}):" \
+               f"\n{questions_all[current_question].replace('@','')}\n\nВыберите один ответ:\n"
+    correct_answer = None
+
+    for index, answer in enumerate(full_base[questions_all[current_question]]):
+        text_msg += f"{index+1}: {answer.replace('$','')}\n"
+        if "$" in answer:
+            correct_answer = index
+
+    return text_msg, len_answers, correct_answer
