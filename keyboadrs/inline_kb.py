e07efeb66a20c85671d29368a395fe25b3afc72e
teirzar <nik.ul@icloud.com>
2023-09-21 14:21:22 +0300

    исправлено: inline_kb.py
    inline_kb.py - исправлены callback'и и имена переменных в kb_inline_testing

--- keyboadrs/inline_kb.py
+++ keyboadrs/inline_kb.py
@@ -12,20 +12,19 @@ def kb_select_session(mode):
     return ikb.as_markup()
 
 
-def kb_inline_testing(mode, len_answers, correct_answer, current, is_studying=False, show=False):
+def kb_inline_testing(mode, len_answers, correct_answer, question, is_studying=False, show=False):
     """Клавиатура тестирования"""
     ikb = InlineKeyboardBuilder()
 
     for i in range(len_answers):
         if i == correct_answer:
-            ikb.button(text=str(i + 1) + ('✅' if show else ''), callback_data=f"test_{mode}_{i}_{current}_correct")
+            ikb.button(text=str(i + 1) + (' ✅' if show else ''), callback_data=f"test_{mode}__{question}_correct")
             continue
-        ikb.button(text=str(i + 1) + ('❌' if show else ''), callback_data=f"test_{mode}_{i}_{current}_mistake")
+        ikb.button(text=str(i + 1) + (' ❌' if show else ''), callback_data=f"test_{mode}__{question}_mistake")
 
     if is_studying:
-        ikb.button(text=("Скрыть" if show else "Показать") + "верный ответ",
-                   callback_data=f"test_{mode}_{'hide' if show else 'show'}_{current}_open",
-                   )
+        ikb.button(text=("Скрыть" if show else "Показать") + " верный ответ",
+                   callback_data=f"test_{mode}_{'hide' if show else 'show'}_{question}_open")
 
     ikb.adjust(len_answers, 1)
     return ikb.as_markup()
